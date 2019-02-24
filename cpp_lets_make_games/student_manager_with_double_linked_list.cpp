37ae574ebacebac69ed01870c7b280ffce8bd590
YJ Cho <reverince@gmail.com>
2019-02-24 11:26:36 +0000

    Add double linked list sort

--- cpp_lets_make_games/student_manager_with_double_linked_list.cpp
+++ cpp_lets_make_games/student_manager_with_double_linked_list.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 const int NAME_SIZE = 32;
 
-enum MENU { NONE, INSERT, DELETE, SEARCH, PRINT, EXIT };
+enum MENU { NONE, INSERT, DELETE, SEARCH, PRINT, SORT, EXIT };
 
 struct Student {
   int id;
@@ -121,12 +121,12 @@ void deleteStudent(List* pList) {
 }
 
 void printStudent(const Student* pStudent) {
-  cout << "학번: " << pStudent->id << '\n';
+  cout << "학번: " << pStudent->id << '\t';
   cout << "이름: " << pStudent->name << '\n';
-  cout << "국어: " << pStudent->kor << '\n';
-  cout << "영어: " << pStudent->eng << '\n';
+  cout << "국어: " << pStudent->kor << '\t';
+  cout << "영어: " << pStudent->eng << '\t';
   cout << "수학: " << pStudent->math << '\n';
-  cout << "총점: " << pStudent->sum << '\n';
+  cout << "총점: " << pStudent->sum << '\t';
   cout << "평균: " << pStudent->avg << '\n';
   cout << '\n';
 }
@@ -170,6 +170,38 @@ void print(List* pList, bool reversed=false) {
   }
 }
 
+void sort(List* pList, bool byID=true) {
+  bool needSwap, isSorted;
+  
+  // 버블 정렬 (학번은 오름차순, 총점은 내림차순)
+  while (!isSorted) {
+    isSorted = true;
+    Node* pFirst = pList->pHead->pNext;
+    Node* pSecond = pFirst->pNext;
+    while (pSecond != pList->pTail) {
+      needSwap = (byID) ?
+        (pFirst->datum.id > pSecond->datum.id) :
+        (pFirst->datum.sum < pSecond->datum.sum);
+      if (needSwap) {
+        isSorted = false;
+        // 이웃 노드 처리
+        pFirst->pPrev->pNext = pSecond;
+        pSecond->pNext->pPrev = pFirst;
+        // pFirst, pSecond 교체
+        pSecond->pPrev = pFirst->pPrev;
+        pFirst->pNext = pSecond->pNext;
+        pSecond->pNext = pFirst;
+        pFirst->pPrev = pSecond;
+      }
+      
+      pFirst = pSecond;
+      pSecond = pSecond->pNext;
+    }
+  }
+  
+  cout << "정렬이 완료되었습니다.\n";
+}
+
 int main() {
   List list;
   int cntStudentID = 1;
@@ -178,7 +210,7 @@ int main() {
   initList(&list);
   
   while (true) {
-    cout << "1. 등록 / 2. 삭제 / 3. 검색 / 4. 출력 / 5. 종료\n";
+    cout << "1. 등록 / 2. 삭제 / 3. 검색 / 4. 출력 / 5. 정렬 / 6. 종료\n";
     cout << "메뉴를 선택하세요: ";
     ipt = inputInt();
     
@@ -204,6 +236,16 @@ int main() {
           if (ipt == 1 || ipt == 2) break;
         }
         print(&list, ipt - 1);
+        break;
+        
+      case SORT:
+        while (true) {
+          cout << "1. 총점 내림차순 / 2. 학번 오름차순\n";
+          cout << "정렬 기준을 선택하세요: ";
+          ipt = inputInt();
+          if (ipt == 1 || ipt == 2) break;
+        }
+        sort(&list, ipt - 1);
         break;
         
       default:
